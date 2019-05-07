475c937d77f6f08cdcf48f9019963cbfb8601e55
dan seo <dan.seo@reisystems.com>
2019-05-07 10:58:27 -0400

    deleted:    README.md 	deleted:    install-openshift.sh 	deleted:    inventory.ini 	new file:   run.sh 	deleted:    user-custom-exports.sh 	deleted:    validate/do.tf 	deleted:    validate/run.sh 	deleted:    validate/start.sh 	deleted:    validate/stop.sh 	deleted:    validate/validate.sh 	deleted:    vol.yaml

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export log=install.log
+cd ~
+
+git clone https://github.com/gshipley/installcentos.git
+
+cd installcentos
+
+export INTERACTIVE=false;  echo $INTERACTIVE
+
+export USERNAME=admin; echo $USERNAME
+            
+export PASSWORD=admin123; echo $PASSWORD
+            
+export PVS=true; echo $PVS
+
+env > $log
+
+# install OKD
+./install-openshift.sh >> $log
+
+exit
